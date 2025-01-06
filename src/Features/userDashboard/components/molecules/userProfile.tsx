432551a6b39f278a887152b9e8df07354a6bb67f
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2025-01-06 09:53:10 +0000

    chore: currently working on the user dashboard

--- src/Features/userDashboard/components/molecules/userProfile.tsx
+++ src/Features/userDashboard/components/molecules/userProfile.tsx
@@ -19,7 +19,7 @@ export const UserProfile: React.FC<UserProfileProps> = ({
   return (
     <Card className=" bg-white rounded-3xl">
       <div className="p-8">
-        <div className="flex justify-between items-start mb-4">
+        <div className="flex justify-between items-start mb-2">
           <h2 className="text-2xl font-cirka text-navy leading-relaxed">
             Hello {userName} welcome to
             <br />
@@ -30,46 +30,47 @@ export const UserProfile: React.FC<UserProfileProps> = ({
             <div className="absolute top-0 right-0 w-2 h-2 bg-green-500 rounded-full" />
           </div>
         </div>
+      </div>
 
-        <div className="space-y-2">
-          <div className="border-b border-gray-100 pb-2">
-            <div className="text-sm text-gray-500 mb-2 font-helvatica">
-              Your Risk Attitude
-            </div>
-            <div className="text-xl text-navyLight font-cirka font-medium">
-              {riskAttitude}
-            </div>
+      <div className="space-y-2 p-2 bg-[#FAFBFB]">
+        <div className="border-b border-[#AAAAAA] pb-2">
+          <div className="text-sm text-gray-500 pl-5  mb-2 font-helvatica">
+            Your Risk Attitude
           </div>
-
-          <div className="border-b border-gray-100 pb-2">
-            <div className="text-sm text-gray-500 mb-2 font-helvatica">
-              Your Current Net Worth
-            </div>
-            <div className="text-xl text-navyLight font-cirka font-medium">
-              {netWorth.toLocaleString()}
-            </div>
+          <div className="text-2xl text-navyLight font-cirka pl-5 font-medium">
+            {riskAttitude}
           </div>
+        </div>
 
-          <div className="border-b border-gray-100 pb-2">
-            <div className="text-sm text-gray-500 mb-2 font-helvatica">
-              Your Investment Experience
-            </div>
-            <div className="text-xl text-navyLight font-cirka font-medium">
-              {investmentExperience}
-            </div>
+        <div className="border-b border-[#AAAAAA] pb-2">
+          <div className="text-sm text-gray-500 pl-5 mb-2 font-helvatica">
+            Your Current Net Worth
+          </div>
+          <div className="text-2xl text-navyLight font-cirka pl-5 font-medium">
+            {netWorth.toLocaleString()}
+          </div>
+        </div>
+
+        <div className="border-b border-[#AAAAAA] pb-2">
+          <div className="text-sm text-gray-500 pl-5 mb-2 font-helvatica">
+            Your Investment Experience
+          </div>
+          <div className="text-2xl text-navyLight font-cirka pl-5 font-medium">
+            {investmentExperience}
           </div>
         </div>
       </div>
-      <div className="pt-2 space-y-2">
-        <div className="flex items-center justify-between bg-gray-50 p-2">
+
+      <div className="pb-2 bg-[#FAFBFB] rounded-b-3xl p-2 space-y-2">
+        <div className="flex items-center border-b justify-between border-[#AAAAAA] p-2 ">
           <div className="flex items-center space-x-4">
-            <div className="w-14 h-14 rounded-xl bg-white flex items-center justify-center">
+            <div className="w-16 h-16 rounded-xl flex items-center justify-center">
               <Image
                 src="/assets/consultation.svg"
                 alt="Consultation"
-                width={40}
-                height={40}
-                className="w-10 h-10"
+                width={48}
+                height={48}
+                className="w-full h-full object-contain"
               />
             </div>
             <span className="text-navy font-helvatica text-sm">
@@ -81,15 +82,15 @@ export const UserProfile: React.FC<UserProfileProps> = ({
           </button>
         </div>
 
-        <div className="flex items-center justify-between bg-gray-50 p-4">
+        <div className="flex items-center border-b justify-between border-[#AAAAAA] p-2">
           <div className="flex items-center space-x-4">
-            <div className="w-14 h-14 rounded-xl bg-white flex items-center justify-center">
+            <div className="w-16 h-16 rounded-xl flex items-center justify-center">
               <Image
                 src="/assets/recommendation.svg"
                 alt="Advisor"
-                width={40}
-                height={40}
-                className="w-10 h-10"
+                width={48}
+                height={48}
+                className="w-full h-full object-contain"
               />
             </div>
             <span className="text-navy font-helvatica text-sm">
@@ -101,15 +102,15 @@ export const UserProfile: React.FC<UserProfileProps> = ({
           </button>
         </div>
 
-        <div className="flex items-center justify-between bg-gray-50 p-4">
+        <div className="flex items-center  justify-between p-2 ">
           <div className="flex items-center space-x-4">
-            <div className="w-14 h-14 rounded-xl bg-white flex items-center justify-center">
+            <div className="w-16 h-16 rounded-xl bg flex items-center justify-center p-1">
               <Image
                 src="/assets/financialDoc.svg"
                 alt="Upload Financial Document"
-                width={40}
-                height={40}
-                className="w-10 h-10"
+                width={48}
+                height={48}
+                className="w-full h-full object-contain"
               />
             </div>
             <span className="text-navy font-helvatica text-sm">
