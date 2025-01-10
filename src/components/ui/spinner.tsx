6c0d49cb8b75bdd8fd7863b5c4546beb131bb38a
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2025-01-10 03:04:19 +0000

    chore: lets deploy to vercel

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,6 +1,10 @@
 import { cn } from "@/lib/utils";
 
-const Spinner = ({ className }: unknown) => {
+interface SpinnerProps {
+  className?: string;
+}
+
+const Spinner = ({ className }: SpinnerProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
